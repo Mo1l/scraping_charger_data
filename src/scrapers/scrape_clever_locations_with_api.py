39cfbb79f6092e0298313609efdf00bc36167974
Mo1l Home PC <nikolajmolllund@hotmail.com>
2025-10-10 11:27:19 +0200

    added a new scraper

---
+++ src/scrapers/scrape_clever_locations_with_api.py
@@ -0,0 +1,46 @@
+from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime
+import re
+import requests
+import numpy as np
+from .base_scraper import base_scraper as Base
+#from selenium.webdriver.common.by import By
+class scraper(Base):
+    def __init__(
+            self, 
+            keyword,
+            station_ids,
+            out_path,
+            url_re:str={},
+            silent=True):
+        # Simply calls the Base init function.
+        super().__init__(
+            keyword=keyword,
+            station_ids=station_ids,
+            out_path=out_path, 
+            url_re=url_re,
+            silent=silent,)
+
+        self.results = {}
+        self.__setup__(silent)
+
+    def __setup__(self, silent):
+        """
+        The setup is part of the initialization of the class
+        and should include any preliminaries before querying the first 
+        url. 
+        If Selenium is used it includes setting up the browser object.
+        If requests is used it can simply be empty.
+        """
+        pass
+    
+    #@profile
+    def run_scrape(self, i, url, scraper_tools):
+        response=requests.get(url)
+        request_time = datetime.now() 
+        
+        # 
+        results=response.json()
+        locationId = 'NotApplicable'
+
+        return locationId, results
